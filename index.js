4ed33c541b712347cf187717582ae46c9af1e617
tram nguyen <tramnguyendalat1989@yahoo.com>
2018-05-09 00:40:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,14 +11,16 @@ function logWhisper(string) {
   console.log(`${string}`.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  var lowerCase = string, upperCase = string
+  var lowerCase = string, upperCase = string, mixedCase = "I love you, Grandma."
   if (lowerCase.toLowerCase() === lowerCase) {
     return "I can't hear you!"
   }
   else if (upperCase.toUpperCase() === upperCase) {
     return "YES INDEED!"
   }
- 
+  else if (mixedCase) {
+    return "I love you, too"
+  }
 }
 
 
